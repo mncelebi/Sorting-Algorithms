4439ec73086b62f3471e795f780dee6dbd354bb7
Kemal <kmlcanaydin@gmail.com>
2018-11-08 21:03:57 +0300

    Edited insertion sort

--- araya_sokma_siralama.cpp
+++ araya_sokma_siralama.cpp
@@ -28,3 +28,26 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+ DEMO
+
+    [5,6,1,3,6,10,2]
+
+    1.Adım : [5|6,1,3,6,10,2]  --> 0.indis(5) sıralı olarak kabul ediliyor
+    2.Adım : [5,6|1,3,6,10,2]  --> 1.indis(6) 6 > 5 olduğu için sıralama değişmiyor. 
+    3.Adım : [5,1,6|3,6,10,2]  --> 2.indis(1) 1 önce 6 ile daha sonra 5 ile karşılaştırılacak. 6 > 1 olduğu için 1 birim kaydırılarak araya sokuluyor.
+    4.Adım : [1,5,6|3,6,10,2]  
+    5.Adım : [1,5,3,6|6,10,2]  --> 3.indis(3) Sırasıyla sonran itibaren karşılaştırılıyor. 6 > 3 olduğu için 1 birim kaydırılarak araya sokuluyor.   
+    6.Adım : [1,3,5,6|6,10,2]
+    7.Adım : [1,3,5,6,6|10,2]
+    8.Adım : [1,3,5,6,6,10|2]  
+    9.Adım : [1,3,5,6,6,2,10]
+    10.Adım : [1,3,5,6,6,2,10]
+    11.Adım : [1,3,5,6,2,6,10]
+    12.Adım : [1,3,5,2,6,6,10]
+    13.Adım : [1,3,2,5,6,6,10]
+    14.Adım : [1,2,3,5,6,6,10] --> Sıralama Tamamlandı.
+
+
+*/
